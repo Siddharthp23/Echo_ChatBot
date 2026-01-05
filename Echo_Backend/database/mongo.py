a42ed23b92a0b442046bf739e1fc92212c74746b
Siddharthp23 <pagaresiddharth239@gmail.com>
2026-01-05 14:57:35 +0530

    fix:mongo.py

--- Echo_Backend/database/mongo.py
+++ Echo_Backend/database/mongo.py
@@ -1,8 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo import MongoClient
 from config import MONGO_URI, DB_NAME
 
-client = MongoClient(MONGO_URI)
+client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
 
 def get_chat_collection():
@@ -14,7 +13,6 @@ counters_collection = db["counters"]
 def get_users_collection():
     return users_collection
 
-
 async def init_db():
     await users_collection.create_index("email", unique=True)
-    await users_collection.create_index("contact_number", unique=False)
+    await users_collection.create_index("contact_number")
